ff32d484e4cff317dc5bf73b7cd866fe8102f532
Hazielpavon <131502216+Hazielpavon@users.noreply.github.com>
2025-06-08 21:31:21 -0600

    Push

--- jugador.h
+++ jugador.h
@@ -1,11 +1,36 @@
 #ifndef JUGADOR_H
 #define JUGADOR_H
 
+#include <vector>
+#include <any>
+#include "entidad.h"
+
 class Jugador
 {
 public:
     Jugador();
 
+    entidad&       getVida()       { return vida; }
+    const entidad& getVida() const { return vida; }
+
+    std::vector<std::any>&       getObjetos()       { return objetos; }
+    const std::vector<std::any>& getObjetos() const { return objetos; }
+
+    std::vector<std::any>&       getLlaves()       { return llaves; }
+    const std::vector<std::any>& getLlaves() const { return llaves; }
+
+    // métodos genéricos para agregar
+    template<typename T>
+    void agregarObjeto(const T& obj) { objetos.push_back(obj); }
+
+    template<typename T>
+    void agregarLlave(const T& llave) { llaves.push_back(llave); }
+
+
+private:
+    entidad               vida;
+    std::vector<std::any> objetos;
+    std::vector<std::any> llaves;
 };
 
 #endif // JUGADOR_H
