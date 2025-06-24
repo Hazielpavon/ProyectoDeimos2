b79ee1d7fb93c0fbdb176daac0155cb96168e279
anisvalle23 <anavalle23@unitec.edu>
2025-06-24 08:15:35 -0600

    ..

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,7 +13,7 @@
 #include "jugador.h"
 #include "mentevacia.h"
 #include "maquina_olvido.h"
-#include "introvideoplayer.h"
+
 /* inventario */
 #include "InventoryWidget.h"
 
@@ -34,53 +34,59 @@ MainWindow::MainWindow(QWidget *parent)
     /* ➋ tu flujo de pantallas original -------------------- */
     pantallaInicio = new PantallaInicio(this);
 
-    connect(pantallaInicio, &PantallaInicio::iniciarJuegoPresionado, this, [=]() {
-        // Mostrar video introductorio
-        auto* intro = new VideoIntro(this);
-        intro->setVideo("resources/historia.mp4");
+    connect(pantallaInicio, &PantallaInicio::iniciarJuegoPresionado, this,
+            [=]()
+            {
+                if (!menuOpciones) {
+                    menuOpciones = new MenuOpciones(this);
 
-        connect(intro, &VideoIntro::videoTerminado, this, [=]() {
-            if (!menuOpciones) {
-                menuOpciones = new MenuOpciones(this);
+                    connect(menuOpciones, &MenuOpciones::nuevaPartida, this,
+                            [=]()
+                            {
+                                /* pantalla de carga */
+                                pantallaCarga = new PantallaCarga(this);
 
-                connect(menuOpciones, &MenuOpciones::nuevaPartida, this, [=]() {
-                    pantallaCarga = new PantallaCarga(this);
+                                connect(pantallaCarga, &PantallaCarga::cargaCompletada,
+                                        this, [=]()
+                                        {
+                                            /* ---------- creamos el jugador ---------- */
+                                            m_player = new Jugador();
+                                            auto& spr = m_player->sprite();
+                                            spr.loadFrames(SpriteState::Walking, ":/resources/0_Blood_Demon_Walking_", 24);
+                                            spr.loadFrames(SpriteState::Idle,    ":/resources/0_Blood_Demon_Idle_",    16);
+                                            spr.loadFrames(SpriteState::IdleLeft,":/resources/0_Blood_Demon_IdleL_",   16);
+                                            spr.loadFrames(SpriteState::WalkingLeft,":/resources/0_Blood_Demon_WalkingL_",24);
+                                            spr.loadFrames(SpriteState::Jump,":/resources/0_Blood_Demon_Jump Loop_",6);
+                                            spr.generateMirroredFrames(SpriteState::Jump, SpriteState::JumpLeft);
+                                            spr.loadFrames(SpriteState::Running,":/resources/0_Blood_Demon_Running_",12);
+                                            spr.generateMirroredFrames(SpriteState::Running, SpriteState::RunningLeft);
+                                            spr.loadFrames(SpriteState::Slashing,":/resources/0_Blood_Demon_Slashing_",12);
+                                            spr.generateMirroredFrames(SpriteState::Slashing, SpriteState::SlashingLeft);
+                                            spr.loadFrames(SpriteState::Slidding,":/resources/0_Blood_Demon_Sliding_",6);
+                                            spr.generateMirroredFrames(SpriteState::Slidding, SpriteState::SliddingLeft);
+                                            spr.loadFrames(SpriteState::dead,":/resources/0_Blood_Demon_Dying_",15);
+                                            spr.generateMirroredFrames(SpriteState::dead, SpriteState::deadleft);
+                                            spr.loadFrames(SpriteState::throwing,":/resources/0_Blood_Demon_Throwing_",12);
+                                            spr.generateMirroredFrames(SpriteState::throwing, SpriteState::throwingLeft);
 
-                    connect(pantallaCarga, &PantallaCarga::cargaCompletada, this, [=]() {
-                        m_player = new Jugador();
-                        auto& spr = m_player->sprite();
-                        spr.loadFrames(SpriteState::Walking, ":/resources/0_Blood_Demon_Walking_", 24);
-                        spr.loadFrames(SpriteState::Idle,    ":/resources/0_Blood_Demon_Idle_",    16);
-                        spr.loadFrames(SpriteState::IdleLeft,":/resources/0_Blood_Demon_IdleL_",   16);
-                        spr.loadFrames(SpriteState::WalkingLeft,":/resources/0_Blood_Demon_WalkingL_",24);
-                        spr.loadFrames(SpriteState::Jump,":/resources/0_Blood_Demon_Jump Loop_",6);
-                        spr.generateMirroredFrames(SpriteState::Jump, SpriteState::JumpLeft);
-                        spr.loadFrames(SpriteState::Running,":/resources/0_Blood_Demon_Running_",12);
-                        spr.generateMirroredFrames(SpriteState::Running, SpriteState::RunningLeft);
-                        spr.loadFrames(SpriteState::Slashing,":/resources/0_Blood_Demon_Slashing_",12);
-                        spr.generateMirroredFrames(SpriteState::Slashing, SpriteState::SlashingLeft);
-                        spr.loadFrames(SpriteState::Slidding,":/resources/0_Blood_Demon_Sliding_",6);
-                        spr.generateMirroredFrames(SpriteState::Slidding, SpriteState::SliddingLeft);
-                        spr.loadFrames(SpriteState::dead,":/resources/0_Blood_Demon_Dying_",15);
-                        spr.generateMirroredFrames(SpriteState::dead, SpriteState::deadleft);
-                        spr.loadFrames(SpriteState::throwing,":/resources/0_Blood_Demon_Throwing_",12);
-                        spr.generateMirroredFrames(SpriteState::throwing, SpriteState::throwingLeft);
+                                            spr.setSize(128,128);
+                                            spr.setState(SpriteState::Idle);
+                                            m_player->transform().setPosition(width()/2.0, height()/2.0);
 
-                        spr.setSize(128,128);
-                        spr.setState(SpriteState::Idle);
-                        m_player->transform().setPosition(width()/2.0, height()/2.0);
 
-                        cargarNivel("Tutorial");
-                    });
 
-                    mostrarPantalla(pantallaCarga);
-                });
-            }
-            mostrarPantalla(menuOpciones);
-        });
 
-        mostrarPantalla(intro);  // ← Mostrar el video al presionar "Iniciar juego"
-    });
+                                            /* -> primer nivel */
+                                            cargarNivel("Tutorial");
+                                        });
+
+
+                                mostrarPantalla(pantallaCarga);
+                            });
+                }
+                mostrarPantalla(menuOpciones);
+            });
+
 }
 
 /* ========================================================= */
@@ -120,11 +126,9 @@ void MainWindow::keyPressEvent(QKeyEvent *ev)
 void MainWindow::cargarNivel(const QString &nombre)
 {
     if (nombre == "Tutorial") {
-        auto* videoIntro = new IntroVideoPlayer("resources/templo_olvido_vid.mp4", [=]() {
-            auto* n = new TutorialScene(m_player, this);
-            mostrarPantalla(n);
-            n->setFocus();
-        }, this);
+        auto* n = new TutorialScene(m_player, this);
+        mostrarPantalla(n);
+        n->setFocus();
     }
     else if (nombre == "TorreDeLaMarca") {
         auto* n = new niveltorredelamarca(m_player, this);
